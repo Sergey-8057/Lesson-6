43196ccccddabd535c7a552a624def2c2b601eb3
Sergey-8057 <117320806+Sergey-8057@users.noreply.github.com>
2022-11-04 12:28:21 +0200

    Lesson-8

---
+++ Birthdays.py
@@ -0,0 +1,96 @@
+from datetime import datetime
+from calendar import monthrange
+
+
+days_week =["Monday", "Tuesday", "Wednesday", "Thursday", "Friday"]
+
+users = [{"name": "Mark", "birthday": datetime(year=1952, month=11, day=4)},
+         {"name": "Jim", "birthday": datetime(year=1967, month=11, day=4)},
+         {"name": "Piter", "birthday": datetime(year=1989, month=11, day=5)},
+         {"name": "Maks", "birthday": datetime(year=1973, month=12, day=4)},
+         {"name": "Ilon", "birthday": datetime(year=2001, month=11, day=6)},
+         {"name": "Sergey", "birthday": datetime(year=1985, month=12, day=31)},
+         {"name": "Nata", "birthday": datetime(year=1996, month=10, day=25)},
+         {"name": "Karl", "birthday": datetime(year=1973, month=11, day=1)}]
+
+def get_birthdays_per_week(users, days_week):
+    current_date = datetime.now() 
+    list_day = {}
+    for user in users:
+        user_date = user.get("birthday") 
+        x = datetime.now()
+        y = str(x.year)
+        m = str(user_date.month)
+        d = str(user_date.day)
+        new_data = str(y + m + d)
+        new_data = datetime.strptime(new_data, '%Y%m%d')
+        day = datetime.weekday(new_data)
+        month = current_date.month
+        days = monthrange(x.year, month)[1]
+        name = user.setdefault("name")
+        if user_date.month == current_date.month: #сравн мес рожд = текущ мес
+            week = current_date.day + 7
+            if user_date.day in range(current_date.day, week):
+                if day < 5:
+                    print_day = days_week[day]
+                    if print_day in list_day:
+                        val_name = list_day.get(print_day)
+                        name = val_name + ', ' + name
+                        list_day[print_day] = name
+                    else:
+                        list_day[print_day] = name
+                else:
+                    print_day = 'Monday'
+                    if print_day in list_day:
+                        val_name = list_day.get(print_day)
+                        name = val_name + ', ' + name
+                        list_day[print_day] = name
+                    else:
+                        list_day[print_day] = name
+        end = current_date.day + 7 - days
+        if end > 0:                         #если дни переходят в след месяц
+            next_month = current_date.month + 1
+            if user_date.month == next_month:
+                if user_date.day in range(1, end):
+                    if day < 5:
+                        print_day = days_week[day]
+                        if print_day in list_day:
+                            val_name = list_day.get(print_day)
+                            name = val_name + ', ' + name
+                            list_day[print_day] = name
+                        else:
+                            list_day[print_day] = name
+                    else:
+                        print_day = 'Monday'
+                        if print_day in list_day:
+                            val_name = list_day.get(print_day)
+                            name = val_name + ', ' + name
+                            list_day[print_day] = name
+                        else:
+                            list_day[print_day] = name
+        if current_date.month == 12 and end > 0:  #если 12 мес и дни переходят в след месяц
+            next_month = 1    
+            if user_date.month == next_month:
+                if user_date.day in range(1, end):
+                    if day < 5:
+                        print_day = days_week[day]
+                        if print_day in list_day:
+                            val_name = list_day.get(print_day)
+                            name = val_name + ', ' + name
+                            list_day[print_day] = name
+                        else:
+                            list_day[print_day] = name
+                    else:
+                        print_day = 'Monday'
+                        if print_day in list_day:
+                            val_name = list_day.get(print_day)
+                            name = val_name + ', ' + name
+                            list_day[print_day] = name
+                        else:
+                            list_day[print_day] = name
+    for key,value in list_day.items():
+        result = key + ': ' + value
+        print(result)
+                
+
+get_birthdays_per_week(users, days_week)
