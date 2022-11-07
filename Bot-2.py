27eeba4c0a299a889902fcbbd158ef9798ad171d
Sergey-8057 <117320806+Sergey-8057@users.noreply.github.com>
2022-11-07 23:52:26 +0200

    Update Bot-2.py

--- Bot-2.py
+++ Bot-2.py
@@ -12,83 +12,143 @@ class Name(Field):
 
 class Phone(Field):
     def __init__(self, phone = None):
-        self.value = phone.split(' ')
+        self.value = phone
 
-    
+
 class AddressBook(UserDict):
-    def add_record(self):
-        self.name = Name(input('Enter name: '))
-        self.phone = Phone(input('Enter phone: '))
-        record = Record(name=self.name.value, phone=self.phone.value).add()
-        print('New contact added!')
+    def add_record(self, record):
+        self.data[record.name.value] = record
 
-    def remove_contact(self):
-        self.name = Name(input('Enter name: '))
-        record = Record(name=self.name.value, phone=self.phone.value).remove()
-        print('Contact removed!')
-
-    def change_contact(self):
-        self.name = Name(input('Enter name: '))
-        self.phone = Phone(input('Enter new phone: '))
-        record = Record(name=self.name.value, phone=self.phone.value).change()
-        print('Contact changed!')
-
-    def find_phone(self):
-        self.name = Name(input('Enter name: '))
-        record = Record(name=self.name.value, phone=self.phone.value).find()
-        print('Contact found!')
-
-    def show_all(self):
-        print(self.data)
-    
 
 class Record:
-    def __init__(self, name, phone):
-        self.name = name.title()
-        self.phone = phone
+    def __init__(self, new_name):
+        self.name = Name(new_name)
+        self.phones = []
 
-    def add(self):
-        book[self.name] = self.phone
+    def add_phone(self, new_phone):        
+        self.phones.append(Phone(new_phone))
 
-    def remove(self):
-        del book[self.name]
+    def change_phone(self, old_phone, new_phone):        
+        for phone in self.phones:
+            if phone.value == old_phone:
+                self.phones.add_phone(new_phone)
+                self.phones.remove_phone(phone)
+            else:
+                print("Phone number doesn't exist")  
 
-    def change(self):
-        book[self.name] = self.phone
+    def remove_phone(self, old_phone):        
+        for phone in self.phones:
+            if phone.value == old_phone:
+                self.phones.remove(phone)
+            else:
+                print("Phone number does't exist")
 
-    def find(self):
-        print(book[self.name])
+    def __repr__(self):        
+        return f'{self.phones}'
 
-def hello_func():
-    print('How can I help you?')
-
-def quit_func():
-    print('Good bay!')
-    quit()
 
 
 book = AddressBook()
 
-def main():
-    commands = {
-        'hello': hello_func,
-        'add': book.add_record,
-        'remove': book.remove_contact,
-        'change': book.change_contact,
-        'phone': book.find_phone,
-        'show all': book.show_all,
-        'good bye': quit_func,
-        'close': quit_func,
-        'exit': quit_func 
-        }
+def get_user_input():
+    user_input = input('Enter command: ').lower().split(' ')
+    return user_input
 
+
+def get_handler(command):
+    return COMMANDS[command]
+
+
+def add_func(user_input):
+    if user_input[1] not in book.data:
+        add_record = Record(user_input[1])
+        add_record.add_phone(user_input[2])
+        book.add_record(add_record)
+        print(f'New contact added')
+    else:
+        add_phone = book.data[1]
+        add_phone.add_phone(user_input[2])
+        print(f'New phone number has been added')
+
+
+def change_func(user_input):
+    if user_input[1] in book.data:
+        old_phone = input('Enter phone number to change: ')
+        renew_phone = book.data[user_input[1]]
+        renew_phone.change_phone(old_phone, user_input[2])
+        print(f'Phone number has been changed')
+    else:
+        print(f"Phone number doesn't exist")
+
+
+def delete_func(user_input):
+    if user_input[1] in book.data:
+        book.data.pop(user_input[1])
+        print(f'Contact has been deleted')
+
+
+def remove_phone_func(user_input):
+     if user_input[1] in book.data:
+        removing = book.data[user_input[1]]
+        removing.remove_phone(user_input[2])
+        print('Phone number has been removed')
+
+
+def phone_func(user_input):
+    if user_input[1] in book.data:
+        print(f'{user_input[1]} has {book.data[user_input[2]]} phone number')
+    else:
+        print('This contact does not exist.')
+
+
+def hello_func(*args):
+     print('How can I help you?')
+    
+
+
+def show_all_func(*args):
+    
+    print(book.data)
+
+
+def quit_func():
+    print(result = 'Good bay!')
+    return result
+
+
+COMMANDS = {
+    'hello': hello_func,
+    'add': add_func,
+    'change': change_func,
+    'phone': phone_func,
+    'delete': delete_func,
+    'remove': remove_phone_func,
+    'show all': show_all_func,
+    'good bye': quit_func,
+    'close': quit_func,
+    'exit': quit_func
+}
+
+def main():
+    print('How can I help you?')
     while True:
-        var = input('Enter commad: ')
-        var = var.lower()
-        if var not in commands:
-            print('Wrong command')
+        user_input = get_user_input()
+
+        if user_input == ['']:
             continue
-        commands[var]()
+        elif user_input[0] == 'show':
+            command = 'show all'
+        else:
+            command = user_input[0]
+
+        handler = get_handler(command)
+        if handler is None:
+            continue
+
+        result = handler(user_input)
+        if result == 'Good bay!':
+            break
+
 
 if __name__ == '__main__':
     main()
